62586f7f977265674dd98a3b9b88801fd38c1ca5
Angela Coates <angela@angelamarii.com>
2017-04-05 22:25:00 -0400

    started off working on classes next

--- app.rb
+++ app.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
 require 'pg'
+require 'sinatra/reloader' if development?
+require 'active_record'
+
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+ActiveRecord::Base.establish_connection(
+adapter: "postgresql",
+database: "tiy-sports"
+)
+
+class Team < ActiveRecord::Base
+  self.primary_key = "id"
+end
+
+after do
+  ActiveRecord::Base.connection.close
+end
 
 get '/' do
+  @teams = Team.all
 
   erb :home
 end
