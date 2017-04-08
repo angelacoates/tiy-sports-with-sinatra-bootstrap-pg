49758be08e67f01765b14d28eb2e1b7f982fc048
Angela Coates <angela@angelamarii.com>
2017-04-08 15:23:57 -0400

    added classes for membership and teams and players, not wanting to budge

--- app.rb
+++ app.rb
@@ -1,20 +1,31 @@
 require 'sinatra'
 require 'pg'
 require 'sinatra/reloader' if development?
-require 'active_record'
+require 'active_record' if development?
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
 ActiveRecord::Base.establish_connection(
 adapter: "postgresql",
 database: "tiy-sports"
 )
+after do
+  ActiveRecord::Base.connection.close
+end
 
 class Team < ActiveRecord::Base
   self.primary_key = "id"
+  has_many :player
+  has_many :teams, through: :players
 end
 
-after do
-  ActiveRecord::Base.connection.close
+class Player < ActiveRecord::Base
+  has_many :memberships
+  has_many :players, through: :memberships
+end
+
+class Membership < ActiveRecord::Base
+  belongs_to :player_id
+  belongs_to :team_id
 end
 
 get '/' do
